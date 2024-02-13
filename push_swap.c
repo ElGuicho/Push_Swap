18b127b857cea4d9f7327db7e85a86f9f03039ae
Guido <guido.mun.her@gmail.com>
2024-02-13 14:39:41 +0100

    little update

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gmunoz <gmunoz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:59:39 by gmunoz            #+#    #+#             */
-/*   Updated: 2024/01/24 14:03:54 by gmunoz           ###   ########.fr       */
+/*   Updated: 2024/02/03 07:39:21 by gmunoz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,39 @@ int	arg_chek(int argc, char **argv, int i)
 	return (1);
 }
 
+int	arg_count(int argc, char **argv, int i)
+{
+	int	j;
+	int	n_args;
+
+	j = 0;
+	n_args = 0;
+	if (argc == 0 || arg_chek(argc, argv, i) == 0)
+		return (0);
+	i = 0;
+	while (i < argc)
+	{
+		n_args++;
+		while (argv[i][j++] != '\0')
+		{
+			if (argv[i][j] == ' ')
+			{
+				n_args++;
+				printf ("%d\n", n_args);
+			}
+		}
+		i++;
+	}
+	printf ("%d\n", n_args);
+	return (n_args);
+}
 int	push_swap(int argc, char **argv)
 {
 	int	i;
 
 	i = 1;
-	if (argc == 0 || arg_chek(argc, argv, i) == 0)
+	if (arg_count(argc, argv, i) == 0)
 		return (0);
+	
 	return (1);
 }
